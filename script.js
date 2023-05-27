4f3a833370ab9432afb8f9d8fcb42b8f7478420b
Susan Gomez <susangomez@Susans-MBP.attlocal.net>
2023-05-27 10:09:29 -0400

    Added a semicolon after the console.log on line 6 in script.js

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ var lastName = " Gomez";
 var age = 21;
 alert("My name is " + firstName + lastName);
 alert("I am " + age + " years old");
-console.log("Although this class is hard, I am learning as I go to hopefully become better! Thank you for helping me BTW!")
+console.log("Although this class is hard, I am learning as I go to hopefully become better! Thank you for helping me BTW!");
